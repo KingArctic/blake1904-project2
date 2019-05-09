dca8d7b706d8cfc23177f643cf5a3676cadda7ea
clyons <austinlyns@gmail.com>
2019-05-09 13:19:22 -0400

    Removed unneeded files.

--- src/model/role.ts
+++
@@ -1,9 +0,0 @@
-
-export class Role {
-  roleId = 0;
-  role = '';
-  constructor(roleId = 0, role = '') {
-    this.roleId = roleId;
-    this.role = role;    
-  }
-}
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,17 +1,6 @@
 import { combineReducers } from "redux";
-import { clickerReducer } from "./clicker.reducer";
-import { chuckNorrisReducer } from "./chuck-norris.reducer";
 import { User } from "../model/user";
 import { authReducer } from "./auth.reducer";
-
-export interface IClickerState {
-  clicks: number
-}
-
-export interface IChuckNorrisState {
-  joke: string,
-  proccessingNewJoke: boolean
-}
 
 export interface IAuthState {
   currentUser?: User,
@@ -19,13 +8,9 @@ export interface IAuthState {
 } 
 
 export interface IState {
-  clicker: IClickerState,
-  chuckNorris: IChuckNorrisState,
   auth: IAuthState
 }
 
 export const state = combineReducers<IState>({
-  clicker: clickerReducer,
-  chuckNorris: chuckNorrisReducer,
   auth: authReducer
 })
