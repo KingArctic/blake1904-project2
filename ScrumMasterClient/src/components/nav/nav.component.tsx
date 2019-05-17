918e22188863b0b74decd1071fde6be0f37230c7
nae-chan <danaemorg@gmail.com>
2019-05-16 20:57:47 -0400

    fixed nav icon links

--- ScrumMasterClient/src/components/nav/nav.component.tsx
+++ ScrumMasterClient/src/components/nav/nav.component.tsx
@@ -28,7 +28,8 @@ export class NavComponent extends React.Component<any, INavBarState> {
 
           <NavItem eventKey="home">
             <NavIcon className="sidenav-icon">
-              <img alt="Home" src="https://img.icons8.com/nolan/45/000000/home.png"></img>
+            <Link to="/home" ><img alt="Home" 
+            src="https://img.icons8.com/nolan/45/000000/home.png"></img></Link>
             </NavIcon>
             <NavText>
               <Link to="/home" > <h5 className="nav_text titles">Home</h5> </Link>
@@ -37,16 +38,18 @@ export class NavComponent extends React.Component<any, INavBarState> {
 
           <NavItem eventKey="userPage">
             <NavIcon>
-            <img alt="User Page" src="https://img.icons8.com/nolan/45/000000/contacts.png"></img>
+            <Link to="/user-page"><img alt="User Page" 
+            src="https://img.icons8.com/nolan/45/000000/contacts.png"></img></Link>
             </NavIcon>
             <NavText>
-              <Link to="/userPage"><h5 className="nav_text titles">My Page</h5></Link>
+              <Link to="/user-page"><h5 className="nav_text titles">My Page</h5></Link>
             </NavText>
           </NavItem>
 
           <NavItem eventKey="questionMaster">
             <NavIcon>
-            <img alt="Question Master" src="https://img.icons8.com/nolan/45/000000/ask-question.png"></img>
+            <Link to="/questionMaster"> <img alt="Question Master" 
+            src="https://img.icons8.com/nolan/45/000000/ask-question.png"></img></Link>
             </NavIcon>
             <NavText>
               <Link to="/questionMaster"><h5 className="nav_text titles">Question Master</h5></Link>
@@ -55,7 +58,7 @@ export class NavComponent extends React.Component<any, INavBarState> {
 
           <NavItem eventKey="codingMaster">
             <NavIcon>
-            <img alt="Coding Master" src="https://img.icons8.com/nolan/45/000000/e-learning.png"></img>
+            <Link to="/codeMaster"><img alt="Coding Master" src="https://img.icons8.com/nolan/45/000000/e-learning.png"></img></Link>
             </NavIcon>
             <NavText>
               <Link to="/codeMaster"><h5 className="nav_text titles">Code Master</h5></Link>
