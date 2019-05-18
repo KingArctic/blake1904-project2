adc4710e6b23d52c22624420b3ee91544a6b08f8
clyons <austinlyns@gmail.com>
2019-05-18 19:10:39 -0400

    working on the scoreboard

--- ScrumMasterClient/src/App.tsx
+++ ScrumMasterClient/src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import './include/bootstrap';
-import { HomeComponent } from './components/home/home.component';
 import SignInComponent from './components/sign-in/sign-in.component';
 import { Provider } from 'react-redux';
 import { store } from './Store';
 import { NavComponent } from './components/nav/nav.component';
 import { SISelectorComponent } from './components/shop-inventory-selector/shop-inventory-selector.component';
+import { ScrollingScoreboardComponent } from './components/scoreboard/scrolling-scoreboard.component';
 
 const App: React.FC = () => {
   return (
@@ -16,7 +16,7 @@ const App: React.FC = () => {
         <NavComponent />
         <div id="main-content-container">
           <Switch>
-            <Route path="/home" component={HomeComponent} />
+            <Route path="/home" component={ScrollingScoreboardComponent} />
             <Route path="/sign-in" component={SignInComponent} />
             <Route component={SISelectorComponent} />
           </Switch>
